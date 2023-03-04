3d634f05a4caa823b18195d272153860881c8de4
yupseth <cristinayup@gmail.com>
2023-03-04 18:42:34 +0200

    Create AddTask component + functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -38,9 +39,18 @@ function App() {
     );
   };
 
+  //add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <header className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask} />
       ) : (
