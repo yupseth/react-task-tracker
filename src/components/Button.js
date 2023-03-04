13627778cfcc4ada17d7bc8c1121462b9c3104ff
yupseth <cristinayup@gmail.com>
2023-03-04 17:44:11 +0200

    Create Button component and fake button functionality

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+const Button = ({ color, text, onClick }) => {
+  return (
+    <div>
+      <button
+        onClick={onClick}
+        style={{ backgroundColor: color }}
+        className="btn"
+      >
+        {text}
+      </button>
+    </div>
+  );
+};
+
+Button.defaultProps = {
+  color: "steelblue",
+};
+
+export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
+import Button from "./Button";
+
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("Click");
+  };
   return (
     <div>
-      <h1>{title}</h1>
+      <h1 className="header">{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </div>
   );
 };
