db09a6a8559fb308faf7120f196e87afdcba5f3b
yupseth <cristinayup@gmail.com>
2023-03-04 17:57:56 +0200

    Make state global

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Go to Vet",
+      day: "Tomorrow at 11:00",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Study React",
+      day: "Today",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Buy something nice",
+      day: "Sunday",
+      reminder: false,
+    },
+  ]);
+
   return (
     <header className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </header>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,4 @@
-import { useState } from "react";
-
-const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Go to Vet",
-      day: "Tomorrow at 11:00",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Study React",
-      day: "Today",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Buy something nice",
-      day: "Sunday",
-      reminder: false,
-    },
-  ]);
+const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
