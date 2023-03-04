999cfe3629fed0401348f7fd9d1dd26eaa7d00c1
yupseth <cristinayup@gmail.com>
2023-03-04 21:10:28 +0200

    Hide add button in About

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import Button from "./Button";
+import { useLocation } from "react-router-dom";
 
 const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button
-        color={showAdd ? "red" : "green"}
-        text={showAdd ? "Closed" : "Add"}
-        onClick={onAdd}
-      />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Closed" : "Add"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
